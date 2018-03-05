01f953d92d348db41c8ecf6f0d759721ad20d45a
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-03-05 10:26:40 -0800

    Added error handling for routes.

--- app.js
+++ app.js
@@ -13,6 +13,23 @@ app.use(jsonParser());
 
 app.use('/questions', routes);
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// Error Handler
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+        error: {
+            message: err.message
+        }
+    });
+});
+
 // Create application server
 var port = process.env.PORT || 3000;
 
--- routes.js
+++ routes.js
@@ -59,7 +59,15 @@ router.delete('/:qid/answers/:aID', function (req, res) {
 // POST /questions/:qID/answers/:aID/vote-up
 // POST /questions/:qID/answers/:aID/vote-down
 // Vote on a specific answer
-router.post('/:qid/answers/:aID/vote-:dir', function (req, res) {
+router.post("/:qid/answers/:aID/vote-:dir", function (req, res, next) {
+        if (req.params.dir.search(/^(up|down)$/) == -1) {
+            var err = new Error('Not Found');
+            err.status = 404;
+            next(err);
+        } else {
+            next();
+        }
+    }, function(req, res){
     res.json({
         response: "You sent me a POST request to /vote-" + req.params.dir,
         questionId: req.params.qID,
