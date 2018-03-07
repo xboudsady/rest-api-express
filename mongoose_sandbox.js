762010ccfd826a1de8102bbadc9ec9022c616ef2
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-03-06 22:02:00 -0800

    Updated mongoose_sandbox.js

--- mongoose_sandbox.js
+++ mongoose_sandbox.js
@@ -17,11 +17,11 @@ db.once("open", function() {
     // Creat database schema
     var Schema = mongoose.Schema;
     var AnimalSchema = new Schema({
-        type: String,
-        size: String,
-        color: String,
-        mass: Number,
-        name: String,
+        type:   {type: String, default: "goldfish"},
+        size:   {type: String, default: "small"},
+        color:  {type: String, default: "godlen"},
+        mass:   {type: Number, default: 0.007},
+        name:   {type: String, default: "Angela"},
     });
 
     // Create a mongoose model
@@ -36,12 +36,36 @@ db.once("open", function() {
         name: "Lawrence"
     });
 
-    // Save our elephant
-    elephant.save(function(err) {
-        if (err) console.error("Save Failed.", err);
-        else console.log("Saved!");
-        db.close(function () {
-            console.log("db connection close");
+    // Create a generic object with our AnimaSchema
+    var animal = new Animal({}); // Goldfish
+
+    // Override default Animal Object
+    var whale = new Animal({
+        type:   "whale",
+        size:   "big",
+        mass:   190500,
+        name:   "Fig"
+    });
+
+    Animal.remove({}, function(err) {
+        if (err) console.error(err);
+        elephant.save(function (err) {
+            if (err) console.error("Save Failed.", err);
+            animal.save(function (err) {
+                if (err) console.error("Save Failed.", err);
+                whale.save(function(err){
+                    if (err) console.error(err);
+                    Animal.find( {size: "big"}, function(err, animals) {
+                        animals.forEach(function(animal) {
+                            console.log(animal.name + " the " + animal.color + " "  + animal.type)
+                        });
+                        db.close(function () {
+                            console.log("db connection close");
+                        });
+                    });
+                });
+            });
         });
     });
 });
+
