73336fd882a173eab5e4a76009e2db3b952228a5
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-03-07 19:20:10 -0800

    Implemented sorting function for voting rating

--- models.js
+++ models.js
@@ -4,6 +4,16 @@ var mongoose = require("mongoose");
 
 var Schema = mongoose.Schema;
 
+var sortAnswers = function(a, b) {
+    //- negative a before b
+    //0 no change
+    //* positive a after b
+    if (a.votes === b.votes) {
+        return b.updatedAt - a.updatedAt;
+    }
+    return b.votes - a.votes;
+};
+
 var AnswerSchema = new Schema ({
     text: String,
     createdAt:  { type: Date, default: Date.now },
@@ -11,10 +21,31 @@ var AnswerSchema = new Schema ({
     votes:      { type: Number, default: 0 }
 });
 
+AnswerSchema.method("update", function(update, callback) {
+    Object.assign(this, updates, { updatedAt: new Date() });
+    this.parent().save(callback);
+});
+
+AnswerSchema.method("vote", function(vote, callback) {
+    if (vote === "up") {
+        this.votes += 1;
+    } else {
+        this.votes -= 1;
+    }
+    this.parent().save(callback);
+});
+
+
 var QuestionSchema = new Schema ({
     text:        String,
     createdAt:   { type: Date, default: Date.now },
     answers:     [AnswerSchema]
+});
+
+
+QuestionSchema.pre("save", function(next) {
+    this.answers.sort(sortAnswers);
+    next();
 });
 
 var Question = mongoose.model("Question", QuestionSchema);
